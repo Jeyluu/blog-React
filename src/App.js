d2f6ceb4a45d68b53ef8c39811906ec43b894c70
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-10-05 17:16:38 +0200

    Initialisation projet ok carte ok

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Containers/Home/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
