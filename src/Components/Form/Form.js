f6aabc4d6ec4229097cf3d778bc049f5d354bf48
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-10-08 11:17:50 +0200

    lien entre le form et le state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
 import './Form.css'
 
 export default function Form() {
+
+
+    const [article, setArticle] = useState({
+        title: "",
+        body: ""
+    })
+
+    const dispatch = useDispatch();
+
+    // Constante qui évitera de raffraichir la page.
+    const handleForm = e => {
+        e.preventDefault();
+    }
+
+    const handleInput = e => {
+        
+        if(e.target.classList.contains('inp-title')) {
+            const newObjState = {...article,title: e.target.value};
+            setArticle(newObjState);
+        } else if(e.target.classList.contains('inp-body')) {
+            const newObjState = {...article,body: e.target.value};
+            setArticle(newObjState);
+        }
+
+    }
+console.log(article);
     return (
         <>
             <h1 className="title-form">Écrirez un article</h1>
 
-            <form className="container-form" >
+            <form onSubmit={handleForm} className="container-form" >
                 <label htmlFor="title">Titre</label>
-                <input type="text" id="title" placeholder="Entrez votre nom" />
+                <input onChange={handleInput} type="text" id="title" placeholder="Entrez votre nom" value={article.title} className="inp-title"/>
 
                 <label htmlFor="article">Votre article</label>
-                <textarea  id="article" placeholder="Votre article"></textarea>
+                <textarea onChange={handleInput} id="article" placeholder="Votre article" value={article.body} className="inp-body"></textarea>
 
-                <button>Envoyer l'article</button>
+                <button >Envoyer l'article</button>
             </form>
         </>
     )
