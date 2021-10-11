49daf8b07834d0c288b72c45f21ea681d79bfcee
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-10-11 11:15:37 +0200

    contact fini

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Contact.css'
 
 export default function Contact() {
     return (
-        <div>
-            
+        <div className="container-contact">
+            <h1>Contactez-nous</h1>
+            <p>Par mail: blog-react@gmail.com</p>
+            <p>Par téléphone: xx xx xx xx xx</p>
+            <p>Rejoingez-nous sur les réseaux</p>
+            <ul>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Youtube</li>
+            </ul>
         </div>
     )
 }
